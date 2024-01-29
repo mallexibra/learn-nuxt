8b5be4c554e12381466bf64781f685e319ab3225
mallexibra <mallexibra@gmail.com>
2024-01-29 08:28:19 +0700

    increment and form input data

---
+++ pages/form.vue
@@ -0,0 +1,29 @@
+<script setup>
+const name = ref("");
+const email = ref("");
+const data = ref([]);
+const submitForm = () => {
+  console.log(`Name: ${name.value}, Email: ${email.value}`);
+  data.value = [{ name: name.value, email: email.value }];
+  console.log(data.value);
+};
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="submitForm">
+      <label for="name">
+        <span>Name:</span>
+        <input class="text-black" type="text" v-model="name" />
+      </label>
+      <br />
+      <label for="email">
+        <span>Email:</span>
+        <input class="text-black" type="text" v-model="email" />
+      </label>
+      <button type="submit">Kirim</button>
+    </form>
+  </div>
+</template>
+
+<style></style>
---
+++ pages/increment.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const i = ref(0);
+const increment = () => i.value++;
+const increment1 = () => (i.value += 2);
+</script>
+
+<template>
+  <div>
+    <p>{{ i }}</p>
+    <button @click="increment1">Tambah 2</button>
+  </div>
+</template>
+
+<style></style>
